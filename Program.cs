dc5a172f9b077c5faadbacf4122fb4d312debeff
kelly519 <94807584+kelly519@users.noreply.github.com>
2021-12-19 20:35:34 +0100

    Berkay Oztoprak

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumberss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool flag;
+
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i < n; i++)
+            {
+                flag = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) Console.WriteLine("{0} ", i);
+            }
+            Console.Read();
+        }
+    }
+}
